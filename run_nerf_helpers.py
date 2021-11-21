9a458d8fca589fe8881a476d21465027aa4dc680
wqrray <jackwqr@126.com>
2021-11-21 13:34:42 -0500

    try to add epsilon to the loss

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 # Misc
-img2mse = lambda x, y : torch.mean((x - y) ** 2)
+img2mse = lambda x, y : torch.mean((x - y + 1e-8) ** 2)
 mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
 to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
 
